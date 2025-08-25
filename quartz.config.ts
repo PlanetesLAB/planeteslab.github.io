d974ed5053bebfd3d560e5c27e5fc6cbfc0d35fa
kmaitreys <km.maitrey@gmail.com>
2025-08-25 15:52:50 +0530

    Quartz sync: Aug 25, 2025, 3:52 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Inter",
-        body: "Inter",
+        header: "IBM Plex Sans",
+        body: "IBM Plex Sans",
         code: "JetBrains Mono",
       },
       colors: {
